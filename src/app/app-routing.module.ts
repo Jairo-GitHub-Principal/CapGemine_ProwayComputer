c86b4c847fc81f6489b1764dfe7880bead4b6ab8
jairocesar <jc200507@gmail.com>
2023-03-16 22:17:58 -0300

    primeiro commit do projeto prowaycomputer da capGemine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { ProdutosRoutingModule } from './produtos/produtos-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+   {path:"",redirectTo:"produtos",pathMatch:"full"}, // ao carregar o projeto, sera redirecionado para pagina inicial que sera aqui definida 
+  {path:"**",component: PaginaNaoEncontradaComponent} // pagina não encontrada, caso a navegação nao encontre a rota requisitada, sera chamado essa rota e abrira a pagina de erro 404 
+];
 
 @NgModule({
+  declarations:[],
+ 
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaginaNaoEncontradaComponent,
+ ],
   imports: [
     BrowserModule,
     AppRoutingModule
